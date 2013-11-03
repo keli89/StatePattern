1d2b3cd23aa0a5c1aac3682671600ef7397024be
like <kli@thoughtworks.com>
2013-11-03 05:19:02 +0200

    init code

---
+++ GumballMachine.cs
@@ -0,0 +1,91 @@
+﻿namespace StatePattern
+{
+    public class GumballMachine
+    {
+        public const string InsertedQuarterMessage = "InsertedQuarterMessage";
+        private const string SoldOutMessage = "SoldOutMessage";
+        private const string SoldMessage = "SoldMessage";
+        private const string SoldCorrectMessage = "SoldCorrectMessage";
+        private const string HasQuarterMessage = "HasQuarterMessage";
+        private const string NoQuarterMessage = "NoQuarterMessage";
+        private const string NoGumballMessage = "NoGumballMessage";
+        private const string DispenseSucessMessage = "DispenseSucessMessage";
+        private const string EjectSucessMessage = "EjectSucessMessage";
+        public int GumballNum { get; private set; }
+
+        public GumballMachine(int gumballNum, MachineStatus machineStatus)
+        {
+            GumballNum = gumballNum;
+            State = machineStatus;
+        }
+
+        public MachineStatus State { get; private set; }
+
+        public string InsertQuarter()
+        {
+            if (State == MachineStatus.SOLD)
+                return SoldMessage;
+            if (State == MachineStatus.SOLD_OUT)
+                return SoldOutMessage;
+            if (State == MachineStatus.HAS_QUARTER)
+                return HasQuarterMessage;
+
+            State = MachineStatus.HAS_QUARTER;
+            return InsertedQuarterMessage;
+        }
+
+        public string TurnCrank()
+        {
+            if (State == MachineStatus.SOLD)
+                return SoldMessage;
+            if (State == MachineStatus.NO_QUARTER)
+                return NoQuarterMessage;
+            if (State == MachineStatus.SOLD_OUT)
+                return SoldOutMessage;
+
+            State = MachineStatus.SOLD;
+            return SoldCorrectMessage;
+        }
+
+        public string Dispense()
+        {
+            if (State == MachineStatus.NO_QUARTER)
+                return NoQuarterMessage;
+            if (State == MachineStatus.HAS_QUARTER)
+                return HasQuarterMessage;
+            if (State == MachineStatus.SOLD_OUT)
+                return SoldOutMessage;
+
+            GumballNum--;
+            if (GumballNum == 0)
+            {
+                State = MachineStatus.SOLD_OUT;
+                return NoGumballMessage;
+            }
+
+            State = MachineStatus.NO_QUARTER;
+            return DispenseSucessMessage;
+        }
+
+        public string EjectQuarter()
+        {
+            if (State == MachineStatus.NO_QUARTER)
+                return NoQuarterMessage;
+            if (State == MachineStatus.SOLD_OUT)
+                return SoldOutMessage;
+            if (State == MachineStatus.SOLD)
+                return SoldMessage;
+
+            State = MachineStatus.NO_QUARTER;
+            return EjectSucessMessage;
+        }
+    }
+
+    public enum MachineStatus
+    {
+        NO_QUARTER = 1,
+        HAS_QUARTER = 2,
+        SOLD_OUT = 3,
+        SOLD
+    }
+}
---
+++ GumballMachineFact.cs
@@ -0,0 +1,55 @@
+﻿using Xunit;
+
+namespace StatePattern
+{
+    public class GumballMachineFact
+    {
+        [Fact]
+        public void gumball_machine_status_should_be_no_quarter_at_start()
+        {
+            var gumballMachine = new GumballMachine(10, MachineStatus.NO_QUARTER);
+            Assert.Equal(MachineStatus.NO_QUARTER, gumballMachine.State);
+        }
+
+        [Fact]
+        public void gumball_machine_status_should_be_has_quarter_if_insert_a_quarter()
+        {
+            var gumballMachine = new GumballMachine(10, MachineStatus.NO_QUARTER);
+            gumballMachine.InsertQuarter();
+            Assert.Equal(MachineStatus.HAS_QUARTER, gumballMachine.State);
+        }
+
+        [Fact]
+        public void gumball_machine_status_should_be_no_quarter_if_eject_quarter_when_status_is_has_quarter()
+        {
+            var gumballMachine = new GumballMachine(10, MachineStatus.HAS_QUARTER);
+            gumballMachine.EjectQuarter();
+            Assert.Equal(MachineStatus.NO_QUARTER, gumballMachine.State);
+        }
+
+        [Fact]
+        public void gumball_machine_status_should_be_sold_if_turn_crank_given_status_is_has_quarter()
+        {
+            var gumballMachine = new GumballMachine(1, MachineStatus.HAS_QUARTER);
+            gumballMachine.TurnCrank();
+            Assert.Equal(MachineStatus.SOLD, gumballMachine.State);
+        }
+
+        [Fact]
+        public void gumball_machine_status_should_back_to_no_quarter_if_dispense_given_has_gumball_and_status_is_sold()
+        {
+            var gumballMachine = new GumballMachine(10, MachineStatus.SOLD);
+            gumballMachine.Dispense();
+            Assert.Equal(gumballMachine.GumballNum, 9);
+            Assert.Equal(MachineStatus.NO_QUARTER, gumballMachine.State);
+        }
+
+        [Fact]
+        public void gumball_machine_status_should_be_sold_out_if_dispense_given_no_gumball_and_status_is_sold()
+        {
+            var gumballMachine = new GumballMachine(1, MachineStatus.SOLD);
+            gumballMachine.Dispense();
+            Assert.Equal(MachineStatus.SOLD_OUT, gumballMachine.State);
+        }
+    }
+}
